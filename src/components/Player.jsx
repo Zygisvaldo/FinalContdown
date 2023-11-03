160963e809cd42d4a05c4406cd05899fcc4150f3
Zygisvaldo <zygiodeze@gmail.com>
2023-11-03 11:26:08 +0100

    fix: reading input and setting name on click with REF

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Player = () => {
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
 
-  const handleChange = (event) => {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  };
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   const handleClick = () => {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   };
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" value={enteredPlayerName} onChange={handleChange} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
