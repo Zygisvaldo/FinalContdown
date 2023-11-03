a1fe5ce202d546117b4f80d2e0bd24a5520133e1
Zygisvaldo <zygiodeze@gmail.com>
2023-11-03 09:02:15 +0100

    initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,12 @@
+const Player = () => {
+  return (
+    <section id="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+};
+export default Player;
