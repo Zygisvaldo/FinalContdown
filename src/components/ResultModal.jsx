0c6c2073b868fafcfb68db3621229e5dfcf0bdac
Zygisvaldo <zygiodeze@gmail.com>
2023-11-04 12:24:05 +0100

    fix: close on esc trigers onReset as it does with close btn

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -6,6 +6,8 @@ const ResultModal = forwardRef(
 
     const userLost = remainingTime <= 0;
     const formatedTimeRemaining = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
     // useImperitiveHandle(ref, function that returns an object with groped properties and methods) exposes functions and properties that can be accessed from outside this comp. Is ment to work together with forwardRef.
 
     // incoming REF will refure to this obeject returened in useImpHndl. So incoming REF is connected to imperitiveHandle
@@ -19,13 +21,14 @@ const ResultModal = forwardRef(
     });
 
     return (
-      <dialog ref={dialog} className="result-modal">
+      <dialog onClose={onReset} ref={dialog} className="result-modal">
         {userLost && <h2>You lost</h2>}
+        {!userLost && <h2>Your Score: {score}</h2>}
         <p>
           The target time was <strong>{targetTime} seconds.</strong>
         </p>
         <p>
-          You stop the timer with{" "}
+          You stop the timer with
           <strong>{formatedTimeRemaining} seconds left.</strong>
         </p>
         {/* in a form with a method="dialog" the button that submits the form will automaticaly close the dialog */}
